8f1354a5ed570a1f7e4f333ff40cfb80165cfa8e
Miao Sun <miaosun88@gmail.com>
2014-03-24 17:17:47 +0000

    processMsg, verifyVersion

--- DistributedBackupService/src/mchannels/MDataRestore.java
+++ DistributedBackupService/src/mchannels/MDataRestore.java
@@ -12,7 +12,24 @@ public class MDataRestore extends MulticastChannel {
 	@Override
 	public void processMsg(String msg) {
 		// TODO Auto-generated method stub
+		System.out.println("Process Restore Message");
+		String[] temp = msg.split(" ");
+		String cmd = temp[0].trim();
+		System.out.println("O commando: " + cmd);
 		
+		if(cmd.equals("CHUNK")) {
+			if(verifyVersion(temp[1].trim())) {
+
+			}
+		}
+	}
+	
+	public Boolean verifyVersion(String version) {
+		if(version.length()==3 && version.substring(1,2).equals('.') && Character.isDigit(version.charAt(0)) && Character.isDigit(version.charAt(2))) {
+			return true;
+		}
+		else
+			return false;
 	}
 
 }
